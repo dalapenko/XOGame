3f03d4dd76fac3438cdbbbbd804b1cd49546dc7e
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:25:46 +0300

    Create Player field and method

--- src/ru/hwodi/boardgame/model/Player.java
+++ src/ru/hwodi/boardgame/model/Player.java
@@ -1,4 +1,19 @@
 package ru.hwodi.boardgame.model;
 
 public class Player {
+    private String name;
+    private Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Figure getFigure() {
+        return this.figure;
+    }
 }
