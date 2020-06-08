388712dde1ac1a6516c6b1aa722356059307dcbc
dalapenko <hwoaran92@yandex.ru>
2020-06-09 01:13:00 +0300

    Release MoveController

--- src/ru/hwodi/xogame/controllers/MoveController.java
+++ src/ru/hwodi/xogame/controllers/MoveController.java
@@ -1,4 +1,13 @@
 package ru.hwodi.xogame.controllers;
 
+import ru.hwodi.xogame.model.Field;
+import ru.hwodi.xogame.model.Figure;
+import ru.hwodi.xogame.model.Point;
+import ru.hwodi.xogame.model.exceptions.AlreadyOccupiedException;
+import ru.hwodi.xogame.model.exceptions.InvalidePointException;
+
 public class MoveController {
+    public void applyFigure(final Field field, final Point point, final Figure figure) throws AlreadyOccupiedException, InvalidePointException {
+        field.setFigure(point, figure);
+    }
 }
