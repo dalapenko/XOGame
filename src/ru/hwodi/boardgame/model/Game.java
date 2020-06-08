496ff36dbf459bd6f47db504aecd3a80274810b8
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:28:49 +0300

    Create Game fields and method

--- src/ru/hwodi/boardgame/model/Game.java
+++ src/ru/hwodi/boardgame/model/Game.java
@@ -1,4 +1,25 @@
 package ru.hwodi.boardgame.model;
 
 public class Game {
+    private Player[] players;
+    private Field field;
+    private String name;
+
+    public Game(Player[] players, Field field, String name) {
+        this.players = players;
+        this.field = field;
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return this.players;
+    }
+
+    public Field getField() {
+        return this.field;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
