498e7cc63090120d93ce815605b3741ea17b524d
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:42:11 +0300

    Create test for Point class

---
+++ test/ru/hwodi/boardgame/model/PointTest.java
@@ -0,0 +1,19 @@
+package ru.hwodi.boardgame.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    Point point = new Point(5, 3);
+
+    @Test
+    public void getX() {
+
+    }
+
+    @Test
+    public void getY() {
+    }
+}
