98a6b70ec9c42ba887c7f18e7f868d4a516cc59c
dalapenko <hwoaran92@yandex.ru>
2020-06-09 00:32:24 +0300

    Add test for WinnerController

---
+++ test/ru/hwodi/xogame/controllers/WinnerControllerTest.java
@@ -0,0 +1,12 @@
+package ru.hwodi.xogame.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WinnerControllerTest {
+
+    @Test
+    public void getWinner() {
+    }
+}
