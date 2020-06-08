46088b10332ddb2ea3ad4dc84973bbfaad915862
dalapenko <hwoaran92@yandex.ru>
2020-06-09 01:32:50 +0300

    Release Main

--- src/ru/hwodi/xogame/Main.java
+++ src/ru/hwodi/xogame/Main.java
@@ -1,4 +1,19 @@
 package ru.hwodi.xogame;
 
+import ru.hwodi.xogame.model.Field;
+import ru.hwodi.xogame.model.Figure;
+import ru.hwodi.xogame.model.Game;
+import ru.hwodi.xogame.model.Player;
+import ru.hwodi.xogame.view.ConsoleView;
+
 public class Main {
+    public static void main(String[] args) {
+        Player player1 = new Player("Volovya", Figure.X);
+        Player player2 = new Player("Tolik", Figure.O);
+        String gameName = "XOGame";
+        Player[] players = {player1, player2};
+        Game game = new Game(players, new Field(), gameName);
+        ConsoleView view = new ConsoleView();
+        view.show(game);
+    }
 }
