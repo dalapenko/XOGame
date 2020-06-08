c9de6aa89c3017404965e61fe897dfbe0bfd00b5
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:59:05 +0300

    Change realize of point class test

--- test/ru/hwodi/boardgame/model/PointTest.java
+++ test/ru/hwodi/boardgame/model/PointTest.java
@@ -10,7 +10,7 @@ public class PointTest {
     public void getX() {
         int expectedX = 5;
 
-        Point point = new Point(expectedX, 3);
+        Point point = new Point(expectedX, 0);
         int actualX = point.getX();
 
         assertEquals(expectedX, actualX);
@@ -20,7 +20,7 @@ public class PointTest {
     public void getY() {
         int expectedY = 5;
 
-        Point point = new Point(3, expectedY);
+        Point point = new Point(0, expectedY);
         int actualY = point.getY();
 
         assertEquals(expectedY, actualY);
