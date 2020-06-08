635701b1bd59e1e2afcfc80b15c515339de56708
dalapenko <hwoaran92@yandex.ru>
2020-06-09 01:28:59 +0300

    Add Main

---
+++ src/ru/hwodi/xogame/Main.java
@@ -0,0 +1,4 @@
+package ru.hwodi.xogame;
+
+public class Main {
+}
--- src/ru/hwodi/xogame/view/ConsoleView.java
+++ src/ru/hwodi/xogame/view/ConsoleView.java
@@ -1,4 +1,34 @@
 package ru.hwodi.xogame.view;
 
+import ru.hwodi.xogame.model.Field;
+import ru.hwodi.xogame.model.Figure;
+import ru.hwodi.xogame.model.Game;
+import ru.hwodi.xogame.model.Point;
+import ru.hwodi.xogame.model.exceptions.InvalidePointException;
+
 public class ConsoleView {
+    public void show(Game game) {
+        Field field = game.getField();
+        int fieldSize = field.getSize();
+        for (int i = 0; i < fieldSize; i++) {
+            printline(field, i);
+        }
+    }
+
+    public void printline(Field field, int row) {
+        int fieldSize = field.getSize();
+
+        for (int i = 0; i < fieldSize; i++) {
+            Point currentPoint = new Point(row, i);
+            Figure figure;
+            try {
+                figure = field.getFigure(currentPoint);
+            } catch (InvalidePointException e) {
+                e.printStackTrace();
+                throw new RuntimeException();
+            }
+            System.out.println((figure != null)?figure:" ");
+            System.out.println("|");
+        }
+    }
 }
