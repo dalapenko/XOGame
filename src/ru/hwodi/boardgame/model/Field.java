d7e6b8cf48d88835d95861d48bdad408000d5ea2
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:36:44 +0300

    Create Field class fields and method

--- src/ru/hwodi/boardgame/model/Field.java
+++ src/ru/hwodi/boardgame/model/Field.java
@@ -1,4 +1,18 @@
 package ru.hwodi.boardgame.model;
 
 public class Field {
+    private final int FIELD_SIZE = 3;
+    private Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize() {
+        return this.FIELD_SIZE;
+    }
+
+    public Figure getFigure(Point point) {
+        return this.field[point.getX()][point.getY()];
+    }
+
+    public void setFigure(Point point, Figure figure) {
+        this.field[point.getX()][point.getY()] = figure;
+    }
 }
