13bd4f72841de33d177cfbe7c5225ecbdae11e4f
dalapenko <hwoaran92@yandex.ru>
2020-06-08 22:58:25 +0300

    Change realize of player class test

--- test/ru/hwodi/boardgame/model/PlayerTest.java
+++ test/ru/hwodi/boardgame/model/PlayerTest.java
@@ -9,7 +9,7 @@ public class PlayerTest {
     @Test
     public void getName() {
         String expectedName = "Volodya";
-        Player player = new Player(expectedName, Figure.X);
+        Player player = new Player(expectedName, null);
         String actualName = player.getName();
 
         assertEquals(expectedName, actualName);
@@ -18,7 +18,7 @@ public class PlayerTest {
     @Test
     public void getFigure() {
         Figure expectedFigure = Figure.X;
-        Player player = new Player("Volodya", expectedFigure);
+        Player player = new Player(null, expectedFigure);
         Figure actualFigure = player.getFigure();
 
         assertEquals(expectedFigure, actualFigure);
